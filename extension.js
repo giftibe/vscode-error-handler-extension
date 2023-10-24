ec4fb4ab7fcd5c2155b1bf6138aff94fc1872873
Gift <Giftibe62@gmail.com>
2023-10-24 12:58:13 +0100

    style: maintaining proper code structure and style

--- extension.js
+++ extension.js
@@ -19,7 +19,6 @@ async function activate(context) {
 	console.log(' "gpt-errors", Terminal Error Detection Extension is now active.');
 
 	const child = spawn('node', ['index.js']);
-
 	let terminalOutput = '';
 
 	// Listen for data on the terminal's stdout
@@ -29,48 +28,55 @@ async function activate(context) {
 		if (terminalOutput.includes('error') || terminalOutput.includes('Error') || terminalOutput.includes('exception')) {
 			try {
 				const errorData = terminalOutput;
-				console.log('Error message detected:', errorData);
-			} catch (error) {
-				console.error('Error appending to error.log:', error);
-			}
-		}
-	});
-
-	// Register a command that copies the terminal output to a file
-	const disposable = vscode.commands.registerCommand('gpt-errors.analyzeError', async () => {
-		if (terminalOutput.includes('error') || terminalOutput.includes('exception') || terminalOutput.includes('Error')) {
-			try {
-				console.log("here now")
-
-				const prompt = PromptTemplate.fromTemplate(`You're a software developer, solving errors displayed on the terminal, good luck with the conversation.`);
-
-				const runnable = prompt.pipe(chat);
-
-				const response = await runnable.invoke({ question: terminalOutput });
-				console.log(response);
-
-				const activeTerminal = vscode.window.activeTerminal;
-
-				if (activeTerminal) {
-					// Send text to the active terminal
-					activeTerminal.show();
-					activeTerminal.sendText(response);
-				} else {
-					// Create or access the terminal
-					const newTerminal = vscode.window.createTerminal('My Terminal');
-
-					// Show content in the terminal
-					newTerminal.show();
-					newTerminal.sendText(response);
-				}
+				const response = await processError(errorData);
+				console.log('Response from OpenAI:', response);
+				displayResponseInTerminal(response);
 
 			} catch (error) {
 				console.log('Error creating solution', error);
 				vscode.window.showErrorMessage('Error analyzing and handling error: ' + error.message);
 			}
-		} else {
-			vscode.window.showInformationMessage('No error detected in the terminal output.');
 		}
+	});
+
+
+	async function processError(errorData) {
+		try {
+			console.log("here now")
+			const prompt = PromptTemplate.fromTemplate(`You're a software developer, solving errors displayed on the terminal, good luck with the conversation.`);
+
+			const runnable = prompt.pipe(chat);
+			const response = await runnable.invoke({ question: errorData });
+			return response
+
+		} catch (error) {
+			console.log('Error creating solution', error);
+			return vscode.window.showErrorMessage('Error analyzing and handling error: ' + error.message);
+		}
+
+	}
+
+
+
+	// Display the response in the terminal function
+	function displayResponseInTerminal(response) {
+		const activeTerminal = vscode.window.activeTerminal;
+
+		if (activeTerminal) {
+			activeTerminal.show();
+			activeTerminal.sendText(response);
+		} else {
+			const newTerminal = vscode.window.createTerminal('My Terminal');
+
+			newTerminal.show();
+			newTerminal.sendText(response);
+		}
+	}
+
+
+	// Register a command that copies the terminal output to a file
+	const disposable = vscode.commands.registerCommand('gpt-errors.analyzeError', async () => {
+		vscode.window.showInformationMessage('"gpt-errors", Terminal Error Detection Extension running.');
 	});
 
 	context.subscriptions.push(disposable);
